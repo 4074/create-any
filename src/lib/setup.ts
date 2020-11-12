f4652cd99f4bcc8c56b309c32c6ac641db129bd6
小峰 <dwfn3398@corp.netease.com>
2020-11-12 15:35:31 +0800

    Support caconfig.json

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -4,17 +4,14 @@ import fs from 'fs-extra'
 import path from 'path'
 import ora from 'ora'
 
-export default async function setup() {
-  const dir = process.cwd()
+function runNpmSetup(dir: string) {
   const packageJsonPath = path.join(dir, 'package.json')
   if (!fs.existsSync(packageJsonPath)) return
 
   const spinner = ora('Setting package.json').start()
 
   // Read package.json from template
-  const packageJson = JSON.parse(
-    fs.readFileSync(packageJsonPath).toString('utf-8')
-  )
+  const packageJson = fs.readJSONSync(packageJsonPath)
 
   const packageJsonChangeKeys = [
     'name',
@@ -28,19 +25,20 @@ export default async function setup() {
     'homepage'
   ]
 
+  // Yarn first
+  const hasYarn = !spawn.sync('yarn', ['-v']).error
+
   // Re-create package.json
   fs.removeSync(packageJsonPath)
-  spawn.sync('npm', ['init', '-y'])
-  const packageJsonInitial = JSON.parse(
-    fs.readFileSync(packageJsonPath).toString('utf-8')
-  )
+  spawn.sync(hasYarn ? 'yarn' : 'npm', ['init', '-y'], { cwd: dir })
+  const packageJsonInitial = fs.readJSONSync(packageJsonPath)
 
   // Generate the final package.json
   for (const key of packageJsonChangeKeys) {
     packageJson[key] = packageJsonInitial[key]
   }
-  const deps = Object.keys(packageJson.dependencies)
-  const devDeps = Object.keys(packageJson.devDependencies)
+  const deps = Object.keys(packageJson.dependencies || {})
+  const devDeps = Object.keys(packageJson.devDependencies || {})
 
   // Install latest dependencies and devDependencies
   packageJson.dependencies = {}
@@ -48,9 +46,50 @@ export default async function setup() {
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
 
   spinner.succeed()
-  console.log()
 
-  if (deps.length) spawn.sync('npm', ['i', ...deps], { stdio: 'inherit' })
-  if (devDeps.length)
-    spawn.sync('npm', ['i', '-D', ...devDeps], { stdio: 'inherit' })
+  if (deps.length) {
+    if (hasYarn) {
+      spawn.sync('yarn', ['add', ...deps], { stdio: 'inherit', cwd: dir })
+    } else {
+      spawn.sync('npm', ['i', ...deps], { stdio: 'inherit', cwd: dir })
+    }
+  }
+
+  if (devDeps.length) {
+    if (hasYarn) {
+      spawn.sync('yarn', ['add', '-D', ...devDeps], {
+        stdio: 'inherit',
+        cwd: dir
+      })
+    } else {
+      spawn.sync('npm', ['i', '-D', ...devDeps], { stdio: 'inherit', cwd: dir })
+    }
+  }
+}
+
+export default async function setup() {
+  const root = process.cwd()
+  let options: { entry?: string[] } = {}
+
+  // Support caconfig.json
+  const configJsonPath = path.join(root, 'caconfig.json')
+  if (fs.existsSync(configJsonPath)) {
+    const configJson = fs.readJSONSync(configJsonPath)
+    options = {
+      ...options,
+      ...configJson
+    }
+  }
+
+  if (options.entry) {
+    if (Array.isArray(options.entry)) {
+      for (const e of options.entry) {
+        runNpmSetup(path.join(root, e))
+      }
+    } else {
+      // TODO: Display error message.
+    }
+  } else {
+    runNpmSetup(root)
+  }
 }
